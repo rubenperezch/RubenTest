e01f2074df4d7c001994b1bc17874459bed2b7c8
Ruben <rubenperezch@gmail.com>
2023-01-21 08:52:26 +0100

    First commit

---
+++ RubenTest/src/main/java/com/example/rubentest/RubenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rubentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubenTestApplication.class, args);
+    }
+
+}
---
+++ RubenTest/src/test/java/com/example/rubentest/RubenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rubentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
